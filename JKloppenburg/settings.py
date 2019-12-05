82ab889fec23cb20b6703110ab0ee6ed254a8487
Jeanette Kloppenburg <Jeanette7@live.com>
2019-12-05 14:05:57 +0100

    De DEBUG waarde gewijzigd van True in environment variabele DJANGO_DEBUG met de waarde False.

--- JKloppenburg/settings.py
+++ JKloppenburg/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('Django_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
