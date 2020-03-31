bf742e529821f42b548ad3bceae1caef6b032953
Jeanette Kloppenburg <Jeanette7@live.com>
2020-03-31 17:30:51 +0200

    Hoofdzakelijk tekstuele aanpassingen.

--- portret/forms.py
+++ portret/forms.py
@@ -8,7 +8,7 @@ from django.core.validators import RegexValidator
 
 class ContactForm(forms.Form):
 #    name = forms.CharField(label='Naam', required = True, max_length = 100, validators = [check_size, ], widget=forms.TextInput(attrs={'placeholder': 'Naam'}))
-    name = forms.CharField(label='Naam', required = True, max_length = 100, validators = [RegexValidator('^[a-zA-Z ]+$', message="Je naam kan alleen uit letters bestaan")], widget=forms.TextInput(attrs={'placeholder': 'Naam'}))
+    name = forms.CharField(label='Naam', required = True, max_length = 100, validators = [RegexValidator('^[a-zA-Z ]+$', message="Je naam kan alleen uit letters bestaan")], widget=forms.TextInput(attrs={'placeholder': 'Je naam'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email adres waarmee ik met jou in contact kan komen'}),required = True)
     message = forms.CharField(label='Bericht', required = True, widget=forms.Textarea(attrs={'placeholder': 'Laat hier je bericht achter'}))
 
