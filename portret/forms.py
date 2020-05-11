9379dcdba1362456d1b4377944c4faa345fd1975
Jeanette Kloppenburg <Jeanette7@live.com>
2020-05-11 14:58:21 +0200

    2 365 data science certificaten toegevoegd en tekst op contact pagina aangepast.

--- portret/forms.py
+++ portret/forms.py
@@ -10,5 +10,5 @@ class ContactForm(forms.Form):
 #    name = forms.CharField(label='Naam', required = True, max_length = 100, validators = [check_size, ], widget=forms.TextInput(attrs={'placeholder': 'Naam'}))
     name = forms.CharField(label='Naam', required = True, max_length = 100, validators = [RegexValidator('^[a-zA-Z ]+$', message="Je naam kan alleen uit letters bestaan")], widget=forms.TextInput(attrs={'placeholder': 'Je naam'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email adres waarmee ik met jou in contact kan komen'}),required = True)
-    message = forms.CharField(label='Bericht', required = True, widget=forms.Textarea(attrs={'placeholder': 'Laat hier je bericht achter'}))
+    message = forms.CharField(label='Bericht', required = True, widget=forms.Textarea(attrs={'placeholder': 'Hier kan je een bericht achterlaten'}))
 
